971e13e9e2b394a2fbf2a5886125d7f4ef735867
Tien Phong <tienphong2203@gmail.com>
2024-11-22 19:46:23 +0700

    Step 6: Create common quiz to display quiz list

---
+++ src/modules/common/quiz/quiz.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz',
+  standalone: true,
+  imports: [],
+  templateUrl: './quiz.component.html',
+  styleUrl: './quiz.component.css'
+})
+export class QuizComponent {
+  @Input() public quizItem: any = {}
+}
