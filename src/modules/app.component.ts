c1a4d0b001fcc3dae2e2279c6d633fcdf3e03ade
Tien Phong <tienphong2203@gmail.com>
2024-11-22 21:21:03 +0700

    Step 11: Create customer layout

--- src/modules/app.component.ts
+++ src/modules/app.component.ts
@@ -3,7 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './common/header/header.component';
 import { FooterComponent } from "./common/footer/footer.component";
 import { QuizComponent } from "./common/quiz/quiz.component";
-import { HomeComponent } from "./home/home.component";
+import { HomeComponent } from "./customer/home/home.component";
 
 @Component({
   selector: 'app-root',
