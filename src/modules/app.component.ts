932977f4f70915230330009fb996c482a3a27b4d
Tien Phong <tienphong2203@gmail.com>
2024-11-22 19:47:56 +0700

    Step 8: Set up view in app component

--- src/modules/app.component.ts
+++ src/modules/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from "./common/footer/footer.component";
+import { QuizComponent } from "./common/quiz/quiz.component";
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, QuizComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
