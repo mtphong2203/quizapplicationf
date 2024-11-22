6939774095c24ac1dceb209257b5f833cfebbe0c
Tien Phong <tienphong2203@gmail.com>
2024-11-22 14:47:47 +0700

    Step 5: Create common footer

---
+++ src/modules/common/footer/footer.component.ts
@@ -0,0 +1,24 @@
+import { DatePipe } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+// Import FontAwesomeModule
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEnvelope, faPhone, faMapLocationDot } from '@fortawesome/free-solid-svg-icons';
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [RouterModule, FontAwesomeModule, DatePipe],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+
+  public logo: string = './assets/images/logo.png'
+
+  faEnvelope = faEnvelope;
+  faPhone = faPhone;
+  faMap = faMapLocationDot;
+
+
+  public dateFooter: Date = new Date(2024, 4, 22);
+}
