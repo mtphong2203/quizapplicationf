6aafbf3d9e682ef21f306f59ca69a5353ee34aa2
Tien Phong <tienphong2203@gmail.com>
2024-11-25 22:02:57 +0700

    Step 14: Using OnInit and Router to get event and transform to navigationend to control event

--- src/modules/common/header/header.component.ts
+++ src/modules/common/header/header.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -9,9 +10,25 @@ import { RouterModule } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  public currentPage: string = '';
 
   public logo: string = './assets/images/logo.png';
   public logoUser: string = './assets/images/avatar-5.png';
+
+  /**
+   *
+   */
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)).subscribe((event: NavigationEnd) => {
+        this.currentPage = event.urlAfterRedirects.split('/')[1];
+      });
+    this.currentPage = this.router.url.split('/')[1];
+  }
+
 
 }
