73f783ccb8cd2ce9b3dac80ddb05bad6dc9332e6
Tien Phong <tienphong2203@gmail.com>
2024-11-26 13:46:06 +0700

    Step 15: Create view contact

--- src/modules/customer/contact/contact.component.ts
+++ src/modules/customer/contact/contact.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEnvelope, faPhone, faMapMarkedAlt } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faYoutube, faTiktok, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, FontAwesomeModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
-
+  public name: string = '';
+  public email: string = '';
+  // icon
+  public faEmail = faEnvelope;
+  public faPhone = faPhone;
+  public faMap = faMapMarkedAlt;
+  // brands
+  public faceBookIcon = faFacebook;
+  public youtubeIcon = faYoutube;
+  public tiktokIcon = faTiktok;
+  public linkedinIcon = faLinkedin;
 }
