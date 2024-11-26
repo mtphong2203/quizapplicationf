5ed82441210431659b772629b3571bc08452afe7
Tien Phong <tienphong2203@gmail.com>
2024-11-26 14:17:38 +0700

    Step 17: Config path manager

--- src/modules/app.routes.ts
+++ src/modules/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { CustomerLayoutComponent } from './layouts/customer-layout/customer-layout.component';
+import { ManagementLayoutComponent } from './layouts/management-layout/management-layout.component';
 
 export const routes: Routes = [
+    {
+        path: 'manager',
+        component: ManagementLayoutComponent
+    },
     {
         path: '',
         component: CustomerLayoutComponent,
