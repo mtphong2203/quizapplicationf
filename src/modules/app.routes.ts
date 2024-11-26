c3d8e492d1151697b0814c9c429b4d1ea7f7763c
Tien Phong <tienphong2203@gmail.com>
2024-11-26 19:44:31 +0700

    Step 21: Config authen path

--- src/modules/app.routes.ts
+++ src/modules/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { CustomerLayoutComponent } from './layouts/customer-layout/customer-layout.component';
 import { ManagementLayoutComponent } from './layouts/management-layout/management-layout.component';
+import { AuthenticationLayoutComponent } from './layouts/authentication-layout/authentication-layout.component';
 
 export const routes: Routes = [
+    {
+        path: 'auth',
+        component: AuthenticationLayoutComponent,
+        loadChildren: () => import('./authentication/authentication.module').then(m => m.AuthenticationModule)
+    },
     {
         path: 'manager',
         component: ManagementLayoutComponent
