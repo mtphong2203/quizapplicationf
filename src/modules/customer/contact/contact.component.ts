aceb6d3548df4994fd150fd7d2b13fc28b7e430c
Tien Phong <tienphong2203@gmail.com>
2024-11-26 19:46:34 +0700

    Step 25: Log feedback to the console

--- src/modules/customer/contact/contact.component.ts
+++ src/modules/customer/contact/contact.component.ts
@@ -1,13 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faEnvelope, faPhone, faMapMarkedAlt } from '@fortawesome/free-solid-svg-icons';
 import { faFacebook, faYoutube, faTiktok, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FormsModule, FontAwesomeModule],
+  imports: [FormsModule, FontAwesomeModule, ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
@@ -23,4 +23,21 @@ export class ContactComponent {
   public youtubeIcon = faYoutube;
   public tiktokIcon = faTiktok;
   public linkedinIcon = faLinkedin;
+
+  public feedBack: FormGroup
+
+  constructor() {
+    this.feedBack = new FormGroup({
+      name: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.email),
+      message: new FormControl('', Validators.maxLength(255)),
+    });
+  }
+
+  public onSubmit(): void {
+    console.log(this.feedBack.value);
+
+  }
+
+
 }
