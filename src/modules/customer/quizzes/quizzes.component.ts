190c6065473531b664b443652d7616e7c786b69b
Tien Phong <tienphong2203@gmail.com>
2024-11-26 13:46:20 +0700

    Step 16: Create view quizzes

--- src/modules/customer/quizzes/quizzes.component.ts
+++ src/modules/customer/quizzes/quizzes.component.ts
@@ -1,15 +1,38 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { QuizComponent } from "../../common/quiz/quiz.component";
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-quizzes',
   standalone: true,
-  imports: [QuizComponent],
+  imports: [QuizComponent, CommonModule, FormsModule],
   templateUrl: './quizzes.component.html',
   styleUrl: './quizzes.component.css'
 })
 export class QuizzesComponent {
 
-  @Input() public quizzes: any = {};
+  public keyword: string = '';
+
+  public quizzes: any[] = [
+    {
+      image: './assets/images/capitals.jpeg',
+      title: 'Capitals of Country',
+      time: 15,
+      description: 'Test your knowledge of country capitals'
+    },
+    {
+      image: './assets/images/inventions.png',
+      title: 'Capitals of Country',
+      time: 90,
+      description: 'Test your knowledge of country capitals'
+    },
+    {
+      image: './assets/images/map.jpeg',
+      title: 'Capitals of Country',
+      time: 5690,
+      description: 'Test your knowledge of country capitals'
+    }
+  ]
 
 }
