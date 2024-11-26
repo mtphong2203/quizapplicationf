cfe5729f8873251a2c5e2ed7d0e3b6fb01438bf3
Tien Phong <tienphong2203@gmail.com>
2024-11-26 19:54:41 +0700

    Step 28: Create base component quiz detail in quiz file

---
+++ src/modules/manager/quiz/quiz-details/quiz-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './quiz-details.component.html',
+  styleUrl: './quiz-details.component.css'
+})
+export class QuizDetailsComponent {
+
+}
